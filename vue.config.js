fb8552230e86d37cd9cbf181ae590404eff37ae2
Ahmad Fajar <ahmad.fajar234@yahoo.com>
2020-12-18 05:20:30 +0700

    Update vue-config

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const mdLoaderOptions = require('./scripts/vue-markdown-loader.conf');
 module.exports = {
     productionSourceMap: false,
     parallel: false,
-    publicPath: process.env.NODE_ENV === 'production' ? '/mdbootstrap-docs/' : '/',
+    publicPath: '/', // process.env.NODE_ENV === 'production' ? '/mdbootstrap-docs/' : '/',
     chainWebpack: (config) => {
         config.resolve.extensions.add('.md');
         config.module.rule('md')
